396d36f6d6bcb9c299fd332f009e2570cddc09f8
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2011-04-07 02:12:40 +0000

    Shuffle hierachy

--- jTessBoxEditor/trunk/src/net/sourceforge/tessboxeditor/GuiWithFont.java
+++ jTessBoxEditor/trunk/src/net/sourceforge/tessboxeditor/GuiWithFont.java
@@ -21,7 +21,7 @@ import java.util.Properties;
 import javax.swing.JOptionPane;
 import net.sourceforge.vietpad.components.FontDialog;
 
-public class GuiWithFont extends GuiWithMRU {
+public class GuiWithFont extends GuiWithCommand {
 
     private Font font;
     private String langCode = "eng";
