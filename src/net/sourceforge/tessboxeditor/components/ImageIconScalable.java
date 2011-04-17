91d5825c3d617f03ddfd347f218402155d18e4d7
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2011-04-17 04:19:02 +0000

    Remove unneeded methods

--- src/net/sourceforge/tessboxeditor/components/ImageIconScalable.java
+++ src/net/sourceforge/tessboxeditor/components/ImageIconScalable.java
@@ -8,7 +8,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import javax.swing.*;
 import java.net.*;
-import javax.imageio.IIOImage;
 
 public class ImageIconScalable extends ImageIcon {
 
@@ -86,67 +85,8 @@ public class ImageIconScalable extends ImageIcon {
         }
     }
 
-    public void setScaledSize(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
-
     public void setScaledFactor(int factor) {
         this.width = getIconWidth() * factor;
         this.height = getIconHeight() * factor;
-    }
-
-    @Override
-    public ImageIconScalable clone() {
-        BufferedImage source = (BufferedImage) this.getImage();
-        BufferedImage copy = new BufferedImage(source.getColorModel(), source.copyData(null), source.isAlphaPremultiplied(), null);
-        return new ImageIconScalable(copy);
-    }
-
-    public ImageIconScalable getRotatedImageIcon(double angle) {
-        double sin = Math.abs(Math.sin(angle));
-        double cos = Math.abs(Math.cos(angle));
-        int w = this.getIconWidth();
-        int h = this.getIconHeight();
-        int newW = (int) Math.floor(w * cos + h * sin);
-        int newH = (int) Math.floor(h * cos + w * sin);
-        GraphicsConfiguration gc = getDefaultConfiguration();
-        BufferedImage result = gc.createCompatibleImage(newW, newH);
-
-        Graphics2D g2d = result.createGraphics();
-        g2d.setColor(UIManager.getColor("Label.background"));
-        g2d.fillRect(0, 0, newW, newH);
-
-        g2d.translate((newW - w) / 2, (newH - h) / 2);
-        g2d.rotate(angle, w / 2, h / 2);
-        g2d.drawRenderedImage((BufferedImage) this.getImage(), null);
-
-        // Alternative for 3 lines above
-//        AffineTransform at = AffineTransform.getRotateInstance(angle, newW / 2, newH / 2);
-//        at.translate((newW - w) / 2, (newH - h) / 2);
-//        g2d.drawRenderedImage((BufferedImage) this.getImage(), at);
-
-        g2d.dispose();
-
-        return new ImageIconScalable(result);
-    }
-
-    private GraphicsConfiguration getDefaultConfiguration() {
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice gd = ge.getDefaultScreenDevice();
-        return gd.getDefaultConfiguration();
-    }
-
-    public static java.util.List<ImageIconScalable> getImageList(java.util.List<IIOImage> iioImageList) {
-        try {
-            java.util.List<ImageIconScalable> al = new java.util.ArrayList<ImageIconScalable>();
-            for (IIOImage iioImage : iioImageList) {
-                al.add(new ImageIconScalable((BufferedImage) iioImage.getRenderedImage()));
-            }
-
-            return al;
-        } catch (Exception e) {
-            return null;
-        }
     }
 }
