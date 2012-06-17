a57d63afc0e95fd1ad1f038e77eaf4922c53294f
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2012-06-17 04:25:32 +0000

    Update instructions and make the script more robust against bad or incomplete input

--- tools/train.ps1
+++ tools/train.ps1
@@ -8,7 +8,7 @@ Automate Tesseract 3.02 language data pack generation process.
 The script file should be placed in the same directory as Tesseract's binary executables.
 All training data files must be prefixed with the language code -- such as: 
 vie.arial.exp0.tif, vie.font_properties, vie.unicharambigs, vie.frequent_words_list, vie.words_list
--- and placed in a trainfolder directory, which could be directly under Tesseract directory.
+-- and placed in a trainfolder directory, which could be placed directly under Tesseract directory.
 
 http://code.google.com/p/tesseract-ocr/wiki/TrainingTesseract3
 
@@ -29,9 +29,9 @@ Windows PowerShell 2.0 Download: http://support.microsoft.com/kb/968929
 if ($args[0] -and ($args[0] -eq "-?" -or $args[0] -eq "-h" -or $args[0] -eq "-help")) {
     Write-Host "Usage: .\train.ps1"
     Write-Host "   or  .\train.ps1 yourlang trainfolder"
-    Write-Host "where trainfolder directory contains all the training data files prefixed with yourlang,"
-    Write-Host "e.g., vie.arial.exp0.tif, vie.font_properties, vie.unicharambigs, vie.frequent_words_list, vie.words_list,"
-    Write-Host "and could be directly under Tesseract directory"
+    Write-Host "where trainfolder directory contains all the training data files prefixed with yourlang, e.g.,"
+    Write-Host "vie.arial.exp0.tif, vie.font_properties, vie.unicharambigs, vie.frequent_words_list, vie.words_list,"
+    Write-Host "and could be placed directly under Tesseract directory"
     exit
 }
 
@@ -46,9 +46,14 @@ if (!$trainDir) {
     $trainDir = Read-Host "Enter location of the training data folder"
 }
 
-if (!(test-path $trainDir))
-{
+if ($lang -eq "" -or $trainDir -eq "") {
+     Write-Host "Invalid input"
+     exit
+}
+
+if (!(test-path $trainDir)) {
     throw "{0} is not a valid path" -f $trainDir
+    exit
 }
 
 echo "=== Generating Tesseract language data for language: $lang ==="
