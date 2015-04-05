5ce3b8596e1af4865050a73589294c56e28e8508
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2015-04-05 01:12:13 +0000

    Pull in improvements by A2K (from GitHub)

--- jTessBoxEditor/trunk/src/net/sourceforge/tessboxeditor/GuiWithSpinner.java
+++ jTessBoxEditor/trunk/src/net/sourceforge/tessboxeditor/GuiWithSpinner.java
@@ -66,9 +66,9 @@ public class GuiWithSpinner extends GuiWithEdit {
 
         Icon icon = jLabelImage.getIcon();
         try {
-            Image subImage = ((BufferedImage) ((ImageIcon) icon).getImage()).getSubimage(rect.x, rect.y, rect.width,rect.height);
+            Image subImage = getSubimage((BufferedImage) ((ImageIcon) icon).getImage(), rect);
             ImageIconScalable subIcon = new ImageIconScalable(subImage);
-            subIcon.setScaledFactor(4);
+            subIcon.setScaledFactor(scaleFactor);
             jLabelSubimage.setIcon(subIcon);
         } catch (Exception e) {
             logger.log(Level.WARNING, e.getMessage(), e);
