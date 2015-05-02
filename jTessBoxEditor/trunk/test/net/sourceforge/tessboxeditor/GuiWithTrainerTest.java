306eff0ecc557aa69ebf90e27d19df98c50c3e32
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2015-05-02 17:25:37 +0000

    Incorporate improvements by A2K to Box View

--- jTessBoxEditor/trunk/test/net/sourceforge/tessboxeditor/GuiWithTrainerTest.java
+++ jTessBoxEditor/trunk/test/net/sourceforge/tessboxeditor/GuiWithTrainerTest.java
@@ -36,8 +36,6 @@ public class GuiWithTrainerTest {
         System.out.println("main");
         String[] args = null;
         GuiWithTrainer.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
