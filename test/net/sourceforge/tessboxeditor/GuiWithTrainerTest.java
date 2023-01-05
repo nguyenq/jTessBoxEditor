16b4fc1762a7ea2fc87f22c64a34a8a5bc7f9370
Quan Nguyen <nguyenq@users.sf.net>
2023-01-04 23:31:40 -0600

    Upgrade Junit

--- test/net/sourceforge/tessboxeditor/GuiWithTrainerTest.java
+++ test/net/sourceforge/tessboxeditor/GuiWithTrainerTest.java
@@ -1,30 +1,28 @@
 package net.sourceforge.tessboxeditor;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 public class GuiWithTrainerTest {
     
     public GuiWithTrainerTest() {
     }
     
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() {
     }
     
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() {
     }
     
-    @Before
+    @BeforeEach
     public void setUp() {
     }
     
-    @After
+    @AfterEach
     public void tearDown() {
     }
 
